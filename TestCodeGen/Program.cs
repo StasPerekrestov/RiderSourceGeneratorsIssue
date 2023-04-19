4c46c5a0741bdcc525ebe0d337235ba934892912
Stanislav Perekrestov <sperekrestov@dmvdesk.com>
2023-04-19 13:42:35 +0200

    initial

---
+++ TestCodeGen/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TestCodeGen;
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+    }
+
+    static partial void HelloFrom(string name);
+}
